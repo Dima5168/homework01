102cdd899c2f80f2018b8cb60c5d5fd36d52ed1a
Dima5168 <128595100+Dima5168@users.noreply.github.com>
2023-04-06 20:08:57 +0500

    Initial commit

---
+++ app/src/main/java/com/example/homework01/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.homework01;
+
+import static com.example.homework01.R.id.motion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ImageView human2 = findViewById(R.id.human2);
+
+        //MotionLayout motionLayout = findViewById(R.id.motion);
+
+
+        //human2.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+         //  public void onClick(View view) {
+          //      motionLayout.transitionToEnd();
+
+          //  }
+       // });
+
+    }
+}
